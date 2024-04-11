70e63b135ba265bdb13f0111f1a44a2147df4bc8
Kiryha <kiryha@gmail.com>
2024-04-11 17:41:28 -0400

    rotate 2 and 9 images

--- assignments/recognizer/recognizer.py
+++ assignments/recognizer/recognizer.py
@@ -1,9 +1,10 @@
 """
+## Recognizer Intro
 Build a neuron network for handwritten digit recognition using existing labeled data sets.
 Data source: www.kaggle.com/c/digit-recognizer
 Data explanation: www.wikiwand.com/en/mnist_database
 
-## Data
+## Training Data
 Source data is a set of 28x28 pixels images of handwritten digits (784 pixel values)
 CSV files rows: digits tata (0, 1, 2, 3 ... 9), 42 000 digit examples
 CSV file columns: label (digit), pixel 0, pixel 1, ... pixel 783
